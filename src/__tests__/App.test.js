7b5267194a207901958f3195936982af3814e732
mmj <mmj@ssb.no>
2020-08-14 11:08:21 +0200

    Suggestions for feature/dataview, including: * Code-splitting * standardJS code formatting style * A little bit easier to read code layout
    Also updated dependencies

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,40 +1,21 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import useAxios from 'axios-hooks'
-
-
+import userEvent from '@testing-library/user-event'
+import { render } from '@testing-library/react'
 
 import App from '../App'
 import { UI } from '../enums'
+
+jest.mock('../components/DataView', () => () => null)
 
 const refetch = jest.fn()
 const errorString = 'A problem occured'
 const errorObject = { response: { data: errorString } }
 
-const testDataString = 'Some data';
-const testDataNestedObjectString = 'nested data';
-const testDataArrayString1 = 'array data 1';
-const testDataArrayString2 = 'array data 2';
-
-const testDataObject = {
-  data: testDataString
-}
-const testDataNestedObject = {
-  data: testDataString,
-  key: {
-    test: testDataNestedObjectString
-  }
-};
-const testDataArray = [
-  testDataArrayString1,
-  testDataArrayString2
-];
-
 const setup = () => {
-  const { getByPlaceholderText, getByText, queryAllByRole, queryByRole } = render(<App />)
+  const { getByPlaceholderText, getByText } = render(<App />)
 
-  return { getByPlaceholderText, getByText, queryAllByRole, queryByRole }
+  return { getByPlaceholderText, getByText }
 }
 
 test('App renders correctly', () => {
@@ -43,50 +24,20 @@ test('App renders correctly', () => {
 
   expect(getByPlaceholderText(UI.PLACEHOLDER).value).toEqual(`${process.env.REACT_APP_API}${UI.SCHEMAS}`)
   expect(getByText(`${UI.VERSION}: ${process.env.REACT_APP_VERSION}`)).toBeInTheDocument()
-
 })
 
-test('App renders with flat response from backend', () => {
-  useAxios.mockReturnValue([{ data: testDataString, loading: false, error: null }, refetch])
-  const { getByPlaceholderText, getByText } = setup()
+test('App renders with response from backend', () => {
+  useAxios.mockReturnValue([{ data: {}, loading: false, error: null }, refetch])
+  const { getByPlaceholderText } = setup()
 
   userEvent.type(getByPlaceholderText(UI.PLACEHOLDER), '/')
-  
-  expect(getByText(testDataString)).toBeInTheDocument()
+
   expect(useAxios).toHaveBeenNthCalledWith(3, `${process.env.REACT_APP_API}${UI.SCHEMAS}/`, { 'manual': true })
-
 })
 
-test('App renders object with response from backend', () => {
-  useAxios.mockReturnValue([{ data: testDataObject, loading: false, error: null }, refetch])
-  const { getByText } = setup()
-  
-  expect(getByText(testDataString)).toBeInTheDocument()
-  expect(useAxios).toHaveBeenNthCalledWith(3, `${process.env.REACT_APP_API}${UI.SCHEMAS}/`, { 'manual': true })
-
-})
-
-test('App renders nested object with response from backend', () => {
-  useAxios.mockReturnValue([{ data: testDataNestedObject, loading: false, error: null }, refetch])
-  const { getByText } = setup()
-
-  expect(getByText(testDataNestedObjectString)).toBeInTheDocument()
-  
-})
-
-test('App renders array with response from backend', () => {
-  useAxios.mockReturnValue([{ data: testDataArray, loading: false, error: null }, refetch])
-  const { getByText } = setup()
-  
-  expect(getByText(testDataArrayString1)).toBeInTheDocument()
-  expect(getByText(testDataArrayString2)).toBeInTheDocument()
-
-})
-
-test('Renders error when backend call returns error', () => {
+test('App renders error when backend call returns error', () => {
   useAxios.mockReturnValue([{ data: undefined, loading: false, error: errorObject }, refetch])
   const { getByText } = setup()
-  
-  expect(getByText(errorString)).toBeInTheDocument()
 
+  expect(getByText(errorString)).toBeInTheDocument()
 })
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
+export { default as DataView } from './DataView'
 export { default as Footer } from './Footer'
